887375d16ce486d5c7d8312525d29ca9a34ea082
marcelovmag <81254324+marcelovmag@users.noreply.github.com>
2022-11-24 01:33:49 -0300

    minor Fixes

---
+++ lib/atuadores.dart
@@ -0,0 +1,5 @@
+class Servos {
+  static double servoX = 0;
+  static double servoY = 0;
+  static bool alarme = false;
+}
--- lib/tela1.dart
+++ lib/tela1.dart
@@ -1,6 +1,6 @@
 import 'package:armadillo_app/preferencia_tema.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
+import 'atuadores.dart';
 
 class Tela1 extends StatelessWidget {
   String servox = ((Servos.servoX * 100).round() / 100).toString();
